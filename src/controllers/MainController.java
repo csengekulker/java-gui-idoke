7d10b1f81cb913b9f41b4142420661ca41a32986
csengekulker <k71608883429@ktch.hu>
2022-06-07 10:25:16 +0200

    set timer

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -1,14 +1,28 @@
 package controllers;
 
+import javax.swing.Timer;
+
 import models.MainModel;
 import views.MainFrame;
 
 public class MainController {
   MainFrame mainFrame;
   MainModel mainModel;
+  Timer timer;
   
   public MainController() {
     this.mainFrame = new MainFrame();
     this.mainModel = new MainModel();
+    this.mainFrame.startButton.addActionListener(action -> startButtonAction());
+    timer = new Timer(500, action -> onTime());
+  }
+
+  private void startButtonAction() {
+    this.timer.start();
+  }
+
+  private void onTime() {
+    // System.out.println("ido van");
+    this.mainFrame.trackPanel.move();
   }
 }
